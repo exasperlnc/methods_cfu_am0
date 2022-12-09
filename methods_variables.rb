8f2db3716a2897a4b3b91303cdce2ea4bc390598
Logan Cole <emtlogancole@gmail.com>
2022-12-09 12:34:24 -0600

    Complete Methods_variables

--- methods_variables.rb
+++ methods_variables.rb
@@ -8,7 +8,12 @@
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
 
+variable = "Logan is neat"
 
+puts variable.upcase
+puts variable.downcase
+puts variable.reverse
+puts variable.length
 
 
 
@@ -23,10 +28,26 @@ last_login = "12/09/2021"
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
+# This method is a lot like the each method, but it works on strings and does something to each character.
+# In this case, it follows each character with an '!' and then prints it to the console. 
+variable.each_char {|var| print var, '!' }
 
+#This method returns a boolean based on if the string is empty or not. 
+# In this case it returns false because the string is not empty. 
+variable.empty?
+
+#This method swaps the cases of each letter from upper to lower, or from lower to upper.
+ puts variable.swapcase
+
+ #Ths method returns an array of the characters in a string. 
+puts variable.chars 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
 
+#It makes it so that the method happens on the object itself, so it changes it permanently. 
 
+puts variable
+variable.swapcase!
+puts variable
 
 
